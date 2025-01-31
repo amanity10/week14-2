1ec0901b250c004c8e32146270e444857bac4b77
Yuliya <yuliya.ivanova@gmail.com>
2025-01-31 15:20:03 +0100

    add modified const

--- index.js
+++ index.js
@@ -8,16 +8,34 @@ console.log(resultOne);
 const resultTwo = newText.replace(/;/g,`\n`);
 console.log(resultTwo);
 
-const substring = resultTwo.replace(`/\s+/g`,'');
+const substring = resultTwo.replace(/\s+/g,'');
 console.log(substring);
 
 const newTextTwo = resultTwo.slice(28,50);
 console.log(newTextTwo);
 
-//console.log(`3. Текст с удалёнными пробелами: ${substring}`);
-//console.log(`4. Извлечение подстроки: ${newText}`);
-//console.log(`5. Замена "клён" на "дубе": ${replacedText}`);
-//console.log(`6. Строка result в верхнем регистре: ${replacedText2}`);
-//console.log(`7. Замена "клён" на "дуб" (все вхождения) в переменной result: ${replacedText3}`);
-//console.log(`8. Индекс первого вхождения "моря": ${index}`);
-//console.log(`9. Строка с измененной первой буквой: ${modifiedText}`);
+const replacedText = newTextTwo.replace("клён","дубе");
+console.log(replacedText);
+
+const replacedText2 = resultTwo.toUpperCase();
+console.log(replacedText2);
+
+const replacedText3 = resultTwo.replace(/клён/g,"дуб");
+console.log(replacedText3);
+
+const index = resultTwo.indexOf(`моря`);
+console.log(index);
+
+
+ const findLetter = replacedText[0];
+ const upperLetter = findLetter.toUpperCase();
+ const slicedText = replacedText.slice(1);
+const modifiedText = (upperLetter + slicedText);
+ console.log(modifiedText);
+ 
+ 
+
+
+
+
+ 
